7a5e2674907e1f57e460c4e7e9b5ceae9b60109e
Karan <karanmahatocse@gmail.com>
2024-11-01 22:34:39 +0530

    db url change

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -1,7 +1,11 @@
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 import { env } from "@/lib/env.mjs";
+import "dotenv/config";
 
+if(!process.env.DATABASE_URL)
+    throw new Error("DATABASE_URL is not defined")
 
-const client = postgres("");
+const client = postgres(process.env.DATABASE_URL);
 export const db = drizzle(client);
+
