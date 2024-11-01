b11b13513411c902bd3a9459dd7199fb273fd322
Karan <karanmahatocse@gmail.com>
2024-11-01 22:46:39 +0530

    change db  url

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -3,9 +3,9 @@ import postgres from "postgres";
 import { env } from "@/lib/env.mjs";
 import "dotenv/config";
 
-if(!process.env.DATABASE_URL)
+if(!process.env.NEXT_PUBLIC_DATABASE_URL)
     throw new Error("DATABASE_URL is not defined")
 
-const client = postgres(process.env.DATABASE_URL);
+const client = postgres(process.env.NEXT_PUBLIC_DATABASE_URL);
 export const db = drizzle(client);
 
