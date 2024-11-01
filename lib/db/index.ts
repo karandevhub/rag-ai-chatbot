526d233b6f349a4003e5d7792c3a5014d2ecac52
Karan <karanmahatocse@gmail.com>
2024-11-01 13:01:35 +0530

    change

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -2,6 +2,9 @@ import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 import { env } from "@/lib/env.mjs";
 
+if (!env.DATABASE_URL) {
+  throw new Error("DATABASE_URL is not defined in the environment variables.");
+}
 const client = postgres(env.DATABASE_URL);
-export const db = drizzle(client);
 
+export const db = drizzle(client);
