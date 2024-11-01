670ac33d0a8f5b1b3f51171434ca0f46013fb59a
Karan <karanmahatocse@gmail.com>
2024-11-01 23:01:32 +0530

    change

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -2,7 +2,7 @@ import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 import "dotenv/config";
 
-let db;
+let db: ReturnType<typeof drizzle> | undefined;
 
 if (process.env.DATABASE_URL) {
     const client = postgres(process.env.DATABASE_URL);
