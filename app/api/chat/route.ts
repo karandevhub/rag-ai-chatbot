cadae541c00bc22074e35364caaa0f2f62e1fe35
Karan <karanmahatocse@gmail.com>
2024-11-13 21:36:18 +0530

    change

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,6 +1,8 @@
+'use server'; 
+import '../../../envConfig'
 import { LangChainStream } from 'ai';
 import { ChatOpenAI } from "@langchain/openai";
-import { vectorStore } from "@/utils/openai";
+import { inMemoryStore, vectorStore } from "@/utils/openai";
 import { NextResponse } from "next/server";
 import { createStuffDocumentsChain } from "langchain/chains/combine_documents";
 import { StringOutputParser } from "@langchain/core/output_parsers";
@@ -21,15 +23,24 @@ export async function POST(req: Request) {
     const llm = new ChatOpenAI({
       modelName: "gpt-3.5-turbo",
       openAIApiKey: process.env.OPENAI_API_KEY,
-      temperature: 0.5, // Lower temperature for more deterministic output
+      temperature: 0.5,
       streaming: true,
       callbacks: [handlers],
     });
 
-    const retriever = vectorStore().asRetriever({
+    const mongoretriever = vectorStore().asRetriever({
       searchType: "mmr",
       searchKwargs: { fetchK: 10, lambda: 0.25 },
     });
+
+
+    const memoryretriver = inMemoryStore.asRetriever({
+      searchType: "mmr",
+      searchKwargs: { fetchK: 10, lambda: 0.25 },
+    });
+
+    console.log("in",memoryretriver)
+    console.log("out",mongoretriever)
 
     const prompt = PromptTemplate.fromTemplate(`
         You are a knowledgeable assistant that provides concise and focused answers based on the provided documents and chat history.
@@ -64,20 +75,23 @@ export async function POST(req: Request) {
     });
 
     const retrievalChain = await createRetrievalChain({
-      retriever: retriever,
+      retriever: memoryretriver,
       combineDocsChain,
     });
 
-    retrievalChain.invoke({
+    
+   const res= retrievalChain.invoke({
       input: currentMessage,
       chat_history: previousMessages,
     });
 
+    console.log(res)
+
     return new Response(stream, {
-      status: 200,
       headers: {
-        "Content-Type": "text/plain; charset=utf-8",
-        "Transfer-Encoding": "chunked",
+        "Content-Type": "text/event-stream",
+        "Cache-Control": "no-cache",
+        Connection: "keep-alive",
       },
     });
   } catch (e) {
