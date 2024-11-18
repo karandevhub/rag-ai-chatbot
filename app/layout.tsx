873f22e33c32a2f3732876576029eae4e5625e48
Karan <karanmahatocse@gmail.com>
2024-11-18 18:32:36 +0530

    test

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { SidebarInset, SidebarProvider } from "@/components/sidebar";
+import { AppSidebar } from "@/components/custom/app-sidebar";
 import { cookies } from "next/headers";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -17,14 +19,18 @@ export default async function RootLayout({
 }>) {
 
 
-
+  const cookieStore = await cookies();
+  const isCollapsed = cookieStore.get('sidebar:state')?.value !== 'true';
   
   return (
     <html lang="en">
       <body className={inter.className}>
-
+      <SidebarProvider defaultOpen={!isCollapsed}>
+        <AppSidebar />
+        <SidebarInset>
           {children}
-
+        </SidebarInset>
+      </SidebarProvider>
       </body>
     </html>
   );
