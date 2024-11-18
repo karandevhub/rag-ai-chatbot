103843dd7d5608b2a35478364cc59d1ce82af971
Karan <karanmahatocse@gmail.com>
2024-11-18 17:19:01 +0530

    test

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
 import { cookies } from 'next/headers';
 import { DEFAULT_MODEL_NAME, models } from '@/ai/models';
-import { generateUUID } from '@/lib/utils';
 import Chat from '../components/Chat';
 
 export default async function Page() {
-    const id = generateUUID();
     const cookieStore = await cookies();
     const modelIdFromCookie = cookieStore.get('model-id')?.value;
     const selectedModelId =
@@ -13,7 +11,6 @@ export default async function Page() {
 
     return (
         <Chat
-            key={id}
             selectedModelId={selectedModelId}
         />
     );
