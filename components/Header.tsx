45b24b2c5440f5e4094d0c91a5239c16bd4747e8
Karan <karanmahatocse@gmail.com>
2024-11-18 18:02:11 +0530

    test

--- components/Header.tsx
+++ components/Header.tsx
@@ -26,7 +26,7 @@ export function ChatHeader({
     return (
         <header className="flex sticky top-0 bg-background py-1.5 items-center px-2 md:px-2 gap-2">
         
-            {(!open || windowWidth < 768) && (
+            {(!false || windowWidth < 768) && (
                 <BetterTooltip content="New Chat">
                     <Button
                         variant="outline"
