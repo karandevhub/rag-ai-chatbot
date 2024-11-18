7da6d3312941aa7fcf5d97378c6566bb2646350e
Karan <karanmahatocse@gmail.com>
2024-11-18 19:12:29 +0530

    test

--- components/custom/side-bar-wrapper.tsx
+++ components/custom/side-bar-wrapper.tsx
@@ -1,13 +1,14 @@
-"use client"
+import { cookies } from "next/headers";
 import { SidebarInset, SidebarProvider } from "../sidebar";
 import { AppSidebar } from "./app-sidebar";
 
-export default  function SideBarWrapper({
+export default async function SideBarWrapper({
     children,
 }: Readonly<{
     children: React.ReactNode;
 }>) {
-let isCollapsed = true;
+    const cookieStore = await cookies();
+    const isCollapsed = cookieStore.get('sidebar:state')?.value !== 'true';
     return (
         <SidebarProvider defaultOpen={!isCollapsed}>
             <AppSidebar />
