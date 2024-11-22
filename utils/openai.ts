6fc122b69d46c4aea409a8c6cc6e63ae710a0be3
Karan <karanmahatocse@gmail.com>
2024-11-23 00:44:48 +0530

    db url change

--- utils/openai.ts
+++ utils/openai.ts
@@ -1,40 +1,39 @@
-import { OpenAIEmbeddings } from "@langchain/openai";
+import { OpenAIEmbeddings } from '@langchain/openai';
 import {
   MongoDBAtlasVectorSearch,
   MongoDBAtlasVectorSearchLibArgs,
-} from "@langchain/mongodb";
-import { MongoClient } from "mongodb";
-import { MemoryVectorStore } from "langchain/vectorstores/memory";
-
+} from '@langchain/mongodb';
+import { MongoClient } from 'mongodb';
+import { MemoryVectorStore } from 'langchain/vectorstores/memory';
 
 let embeddingsInstance: OpenAIEmbeddings | null = null;
 
 export const client = new MongoClient(
-  "mongodb+srv://daredevil15963:U8sPkw3zv88aPgG1@cluster0.nni1s.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+  process.env.MONGODB_URI || 'bdsfdbsfbskdbfksbdkfbskdbfksbdfkbsdjfbskjdbfsbdf'
 );
 await client.connect();
 
-const namespace = "chatter.training_data";
-const [dbName, collectionName] = namespace.split(".");
+const namespace = 'chatter.training_data';
+const [dbName, collectionName] = namespace.split('.');
 export const collection = client.db(dbName).collection(collectionName);
 
 export function getEmbeddingsTransformer(): OpenAIEmbeddings {
   try {
     if (!embeddingsInstance) {
       embeddingsInstance = new OpenAIEmbeddings({
-        openAIApiKey:process.env.OPENAI_API_KEY ?? "snkkd"
+        openAIApiKey: process.env.OPENAI_API_KEY ?? 'snkkd',
       });
     }
     return embeddingsInstance;
   } catch (error) {
-    console.error("Error creating OpenAIEmbeddings instance:", error);
-    console.error("Retrying creation of OpenAIEmbeddings...");
+    console.error('Error creating OpenAIEmbeddings instance:', error);
+    console.error('Retrying creation of OpenAIEmbeddings...');
     embeddingsInstance = new OpenAIEmbeddings({
-      openAIApiKey: process.env.OPENAI_API_KEY ?? "snkkd"
+      openAIApiKey: process.env.OPENAI_API_KEY ?? 'snkkd',
     });
     if (!embeddingsInstance) {
       throw new Error(
-        "Failed to create OpenAIEmbeddings instance after retries. Check the logs for details."
+        'Failed to create OpenAIEmbeddings instance after retries. Check the logs for details.'
       );
     }
 
@@ -44,8 +43,8 @@ export function getEmbeddingsTransformer(): OpenAIEmbeddings {
 
 export function vectorStore(): MongoDBAtlasVectorSearch {
   const vectorStore: MongoDBAtlasVectorSearch = new MongoDBAtlasVectorSearch(
-    new OpenAIEmbeddings( {
-      openAIApiKey:process.env.OPENAI_API_KEY ?? "snkkd"
+    new OpenAIEmbeddings({
+      openAIApiKey: process.env.OPENAI_API_KEY ?? 'snkkd',
     }),
     searchArgs()
   );
@@ -54,18 +53,20 @@ export function vectorStore(): MongoDBAtlasVectorSearch {
 
 export let inMemoryStore: MemoryVectorStore = new MemoryVectorStore(
   new OpenAIEmbeddings({
-    openAIApiKey:process.env.OPENAI_API_KEY ?? "snkkd"
+    openAIApiKey: process.env.OPENAI_API_KEY ?? 'snkkd',
   })
 );
 export async function inMemoryVectorStore(): Promise<MemoryVectorStore> {
   try {
-    inMemoryStore = new MemoryVectorStore(new OpenAIEmbeddings({
-      openAIApiKey:process.env.OPENAI_API_KEY ?? "snkkd"
-    }));
-    console.log("Memory Vector Store initialized successfully.");
+    inMemoryStore = new MemoryVectorStore(
+      new OpenAIEmbeddings({
+        openAIApiKey: process.env.OPENAI_API_KEY ?? 'snkkd',
+      })
+    );
+    console.log('Memory Vector Store initialized successfully.');
     return inMemoryStore;
   } catch (error) {
-    console.error("Error initializing Memory Vector Store:", error);
+    console.error('Error initializing Memory Vector Store:', error);
     throw error;
   }
 }
@@ -73,9 +74,9 @@ export async function inMemoryVectorStore(): Promise<MemoryVectorStore> {
 export function searchArgs(): MongoDBAtlasVectorSearchLibArgs {
   const searchArgs: MongoDBAtlasVectorSearchLibArgs = {
     collection: collection,
-    indexName: "vector_index",
-    textKey: "text",
-    embeddingKey: "text_embedding",
+    indexName: 'vector_index',
+    textKey: 'text',
+    embeddingKey: 'text_embedding',
   };
   return searchArgs;
 }
